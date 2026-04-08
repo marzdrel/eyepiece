570547524c0b36c9a0eec506f4fb0f84b8198880
Marz Drel <marzdrel@users.noreply.github.com>
2026-04-08 14:48:55 +0200

    feat: Add QuickSearch and QuickBriefScope modules
      - QuickSearch adds .like() and .mlike() class methods for
        PostgreSQL ILIKE searches across multiple fields
      - QuickBriefScope adds a configurable .brief() scope using
        reselect for concise console output
      - Both are parameterized Module subclasses included into
        ActiveRecord models

---
+++ lib/eyepiece.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "eyepiece/version"
+require_relative "eyepiece/quick_search"
+require_relative "eyepiece/quick_brief_scope"
+
+module Eyepiece
+  class Error < StandardError; end
+end
---
+++ spec/eyepiece_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe Eyepiece do
+  it "has a version number" do
+    expect(Eyepiece::VERSION).not_to be_nil
+  end
+end
